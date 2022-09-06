7153cc6d79c38a84399939fdc94bd92277f91918
Timi <timeaher@gmail.com>
2022-09-06 15:09:48 +0200

    starter design

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,7 @@ const server = http.createServer((req, res) => {
 		res.end(errorHTML);
 	}else{
 		if(fs.statSync(filePath).isDirectory()) {
-			filePath += 'frontend/index.html';
+			filePath += '/index.html';
 		}
 		fs.readFile(filePath, "binary", (err, data) => {
 			if(err) {
