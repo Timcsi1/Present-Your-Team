ccfe7afe6074792995a7875413483cfba58c0f69
Daniel Lendvai <lendvai.daniel92@gmail.com>
2022-09-06 09:35:14 +0200

    array iteration

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // JSON string
 let txt =
     '{"classMates":[' +
-    '{"firstName":"Daniel","lastName":"Lendvai","education":"Szent Istvan University","motivation":"Building good-looking stuff from scracth"},' +
+    '{"firstName":"Daniel","lastName":"Lendvai","education":"Szent Istvan University","motivation":"Building good-looking stuff from scratch"},' +
     '{"firstName":"Anna","lastName":"Smith","education":"School","motivation":"Lorem ipsum"},' +
     '{"firstName":"Johannis","lastName":"Jones","education":"School","motivation":"Lorem ipsum"},' +
     '{"firstName":"Robert","lastName":"Garry","education":"School","motivation":"Lorem ipsum"},' +
@@ -16,3 +16,13 @@ document.getElementById("fullName").innerHTML =
 document.getElementById("education").innerHTML = obj.classMates[0].education;
 document.getElementById("motivation").innerHTML = obj.classMates[0].motivation;
 
+
+// practice - should be useful when creating layout
+// iterate "classMates" and log the data.  
+for (const i of obj.classMates) {
+    console.log(i.firstName + " " + i.lastName + " - " + (i.education));
+    console.log(i.motivation);
+}
+// convert json to javascript Map
+const map = new Map(Object.entries(JSON.parse(txt)));
+console.log(map);
