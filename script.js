1fa6b478bbad6f9b1d6f1c8e606a53d7ef14401b
Daniel Lendvai <lendvai.daniel92@gmail.com>
2022-09-06 13:26:58 +0200

    select elements by class

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
+let images = [];
+images.push("", "", "", "", "");
+
 // JSON string
 let txt =
     '{"classMates":[' +
     '{"firstName":"Daniel","lastName":"Lendvai","education":"Szent Istvan University","motivation":"Building good-looking stuff from scratch"},' +
-    '{"firstName":"Mark","lastName":"Smith","education":"School","motivation":"Lorem ipsum"},' +       
-    '{"firstName":"Johannis","lastName":"Jones","education":"School","motivation":"Lorem ipsum"},' +    
-    '{"firstName":"Robert","lastName":"Garry","education":"School","motivation":"Lorem ipsum"},' +      
+    '{"firstName":"Mark","lastName":"Smith","education":"School","motivation":"Lorem ipsum"},' +
+    '{"firstName":"Johannis","lastName":"Jones","education":"School","motivation":"Lorem ipsum"},' +
+    '{"firstName":"Robert","lastName":"Garry","education":"School","motivation":"Lorem ipsum"},' +
     '{"firstName":"Lazlov","lastName":"Abraham","education":"School","motivation":"Lorem ipsum"}]}';
 
-//  converts text into a JavaScript object 
-let obj = JSON.parse(txt);
+let obj = JSON.parse(txt); //  converts text into a JavaScript object
 
-// changing the html element by id //should be changed to class
-document.getElementById("fullName").innerHTML =
+// changing the html element by class
+document.querySelector(".fullName").innerHTML =
     obj.classMates[0].firstName + " " + obj.classMates[0].lastName;
-document.getElementById("education").innerHTML = obj.classMates[0].education;
-document.getElementById("motivation").innerHTML = obj.classMates[0].motivation;
-
-
-// practice - should be useful when creating layout
-// iterate "classMates" and log the data.  
-for (const i of obj.classMates) {
-    console.log(i.firstName + " " + i.lastName + " - " + (i.education));
-    console.log(i.motivation);
-}
-// convert json to javascript Map
-const map = new Map(Object.entries(JSON.parse(txt)));
-console.log(map);
+document.querySelector(".education").innerHTML = obj.classMates[0].education;
+document.querySelector(".motivation").innerHTML = obj.classMates[0].motivation;
