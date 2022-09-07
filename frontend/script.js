f54b273a60222d4f1adf952cc238bdce9397b3b9
Daniel Lendvai <lendvai.daniel92@gmail.com>
2022-09-07 09:51:29 +0200

    script.js edited, now working

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 // JSON string
 let txt =
     '{"classMates":[' +
-    '{"firstName":"Daniel","lastName":"Lendvai","education":"Szent Istvan University","motivation":"Building good-looking stuff from scratch","workingfields":"Tourism"},' +
+    '{"firstName":"Daniel","lastName":"Lendvai","education":"Szent Istvan University","motivation":"Building good-looking stuff from scratch","workingfield":"Tourism","skill":"loremipsum","interest":"loremipsummipsum","highlight":"loremipsum","movie":"loremipsum","music":"loremipsum","videogame":"loremipsum"},' +
     '{"firstName":"Mark","lastName":"Smith","education":"School","motivation":"Lorem ipsum"},' +
     '{"firstName":"Johannis","lastName":"Jones","education":"School","motivation":"Lorem ipsum"},' +
     '{"firstName":"Robert","lastName":"Garry","education":"School","motivation":"Lorem ipsum"},' +
@@ -9,18 +9,44 @@ let txt =
 
 let obj = JSON.parse(txt); //  converts text into a JavaScript object
 
-let p = window.location.pathname;   //checks which page is running
-if (p.includes("daniellendvai")) {  //if it contains "daniellenvai" then display 
+let p = window.location.pathname; //checks which page is running
+if (p.includes("daniellendvai")) {
+    //if it contains "daniellenvai" then display
     document
         .querySelector(".education")
         .appendChild(document.createElement("li")).textContent =
         obj.classMates[0].education;
     document
-        .querySelector(".working-fields")
+        .querySelector(".working-field")
         .appendChild(document.createElement("li")).textContent =
-        obj.classMates[0].workingfields;
-} 
-if (p.includes("sajatneved")) {    //if it contains "sajatneved" then display 
+        obj.classMates[0].workingfield;
+    document
+        .querySelector(".skill")
+        .appendChild(document.createElement("li")).textContent =
+        obj.classMates[0].skill;
+    document
+        .querySelector(".interest")
+        .appendChild(document.createElement("li")).textContent =
+        obj.classMates[0].interest;
+    document
+        .querySelector(".highlight")
+        .appendChild(document.createElement("li")).textContent =
+        obj.classMates[0].highlight;
+    document
+        .querySelector(".movie")
+        .appendChild(document.createElement("li")).textContent =
+        obj.classMates[0].movie;
+    document
+        .querySelector(".music")
+        .appendChild(document.createElement("li")).textContent =
+        obj.classMates[0].music;
+    document
+        .querySelector(".videogame")
+        .appendChild(document.createElement("li")).textContent =
+        obj.classMates[0].videogame;
+}
+if (p.includes("sajatneved")) {
+    //if it contains "sajatneved" then display
     document
         .querySelector(".education")
         .appendChild(document.createElement("li")).textContent =
@@ -29,5 +55,5 @@ if (p.includes("sajatneved")) {    //if it contains "sajatneved" then display
         .querySelector(".working-fields")
         .appendChild(document.createElement("li")).textContent =
         obj.classMates[0].workingfields;
-} 
+}
 console.log(p);
