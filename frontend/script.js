14c5747818ef0e52727724f3c90b1621dc5ef74d
Daniel Lendvai <lendvai.daniel92@gmail.com>
2022-09-07 09:18:09 +0200

    playin around with scripts, edited personal page

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,7 @@
-let images = [];
-images.push("", "", "", "", "");
-
 // JSON string
 let txt =
     '{"classMates":[' +
-    '{"firstName":"Daniel","lastName":"Lendvai","education":"Szent Istvan University","motivation":"Building good-looking stuff from scratch"},' +
+    '{"firstName":"Daniel","lastName":"Lendvai","education":"Szent Istvan University","motivation":"Building good-looking stuff from scratch","workingfields":"Tourism"},' +
     '{"firstName":"Mark","lastName":"Smith","education":"School","motivation":"Lorem ipsum"},' +
     '{"firstName":"Johannis","lastName":"Jones","education":"School","motivation":"Lorem ipsum"},' +
     '{"firstName":"Robert","lastName":"Garry","education":"School","motivation":"Lorem ipsum"},' +
@@ -12,8 +9,6 @@ let txt =
 
 let obj = JSON.parse(txt); //  converts text into a JavaScript object
 
-// changing the html element by class
-document.querySelector(".fullName").innerHTML =
-    obj.classMates[0].firstName + " " + obj.classMates[0].lastName;
-document.querySelector(".education").innerHTML = obj.classMates[0].education;
-document.querySelector(".motivation").innerHTML = obj.classMates[0].motivation;
+document.querySelector(".education").appendChild(document.createElement('li')).textContent = obj.classMates[0].education;
+
+document.querySelector(".working-fields").appendChild(document.createElement('li')).textContent = obj.classMates[0].workingfields;
